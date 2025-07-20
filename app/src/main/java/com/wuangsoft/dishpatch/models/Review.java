d2344fdbde33a144c1c6ce0027e55b91d435bff9
longvvhe181579 <longvvhe181579@fpt.edu.vn>
2025-07-20 18:23:05 +0700

    Implement Shared Preference

--- app/src/main/java/com/wuangsoft/dishpatch/models/Review.java
+++ app/src/main/java/com/wuangsoft/dishpatch/models/Review.java
@@ -2,16 +2,40 @@ package com.wuangsoft.dishpatch.models;
 
 public class Review {
     private float rating;
+    private String comment;
     private String itemId;
-
+    private String userId;
     public Review() {}
+    public Review(float rating, String comment, String itemId, String userId) {
+        this.rating = rating;
+        this.comment = comment;
+        this.itemId = itemId;
+        this.userId = userId;
+    }
 
     public float getRating() {
         return rating;
     }
 
-    public void setRating(float rating) {
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+public void setRating(float rating) {
+        if (rating < 0 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 0 and 5");
+        }
         this.rating = rating;
+}
+
+
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
     public String getItemId() {
